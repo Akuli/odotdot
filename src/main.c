2438cc9ad8d756a252edb568f44f77c344626be3
Akuli <akuviljanen17@gmail.com>
2018-03-31 16:44:08 +0300

    parse

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "ast.h"
 #include "common.h"
+#include "dynamicarray.h"
 #include "tokenizer.h"
 #include "unicode.h"
+
+// functions can't be casted because c standards
+static void astnode_free_voidstar(void *node)
+{
+	astnode_free(node);    // casts the node to struct AstNode*
+}
+
 
 int main(int argc, char **argv)
 {
@@ -41,11 +50,11 @@ int main(int argc, char **argv)
 	free(hugestr);
 
 	if (status == 1) {
-		fprintf(stderr, "%s: error while decoding '%s' as UTF-8: %s\n", argv[0], argv[1], errormsg);
+		fprintf(stderr, "%s: the content of '%s' is not valid UTF-8: %s\n", argv[0], argv[1], errormsg);
 		return 1;
 	}
 	if (status == STATUS_NOMEM) {
-		fprintf(stderr, "%s: ran out of memory while UTF8-decoding '%s'\n", argv[0], argv[1]);
+		fprintf(stderr, "%s: ran out of memory while UTF8-decoding the content of '%s'\n", argv[0], argv[1]);
 		return 1;
 	}
 	assert(status == STATUS_OK);
@@ -54,19 +63,32 @@ int main(int argc, char **argv)
 	struct Token *tok1st = token_ize(hugeunicode);
 	free(hugeunicode.val);
 
-	// TODO: parse and run instead of dumping the tokens to stdout
-	for (struct Token *tok = tok1st; tok; tok = tok->next) {
-		char errormsg[100]={0};
-		char *tokval;
-		size_t tokvallen;
-		assert(utf8_encode(tok->str, &tokval, &tokvallen, errormsg) == STATUS_OK);
-		assert((tokval = realloc(tokval, tokvallen+1)));
-		tokval[tokvallen]=0;
-		assert(!errormsg[0]);
-		printf("%llu\t%c\t%s\n", (unsigned long long) tok->lineno, tok->kind, tokval);
-		free(tokval);
+	// parse
+	struct DynamicArray *statements = dynamicarray_new();
+	if (!statements) {
+		fprintf(stderr, "%s: ran out of memory while parsing the content of '%s'\n", argv[0], argv[1]);
+		token_freeall(tok1st);
+		return 1;
 	}
 
+	struct Token *curtok = tok1st;
+	while (curtok) {
+		struct AstNode *st = parse_statement(&curtok);
+		if (!st) {
+			// TODO: better error reporting... also fix ast.c
+			fprintf(stderr, "%s: something went wrong with parsing the content of '%s'\n", argv[0], argv[1]);
+			token_freeall(tok1st);
+			return 1;
+		}
+		dynamicarray_push(statements, st);
+	}
 	token_freeall(tok1st);
+
+	// TODO: run it instead of this
+	if (statements->len == 1)
+		printf("there is 1 statement\n");
+	else
+		printf("there are %llu statements\n", (unsigned long long) statements->len);
+	dynamicarray_freeall(statements, astnode_free_voidstar);
 	return 0;
 }
