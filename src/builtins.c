31ef7ef2e9fcfa31da022af648887f863d20a5b3
Akuli <akuviljanen17@gmail.com>
2018-05-27 12:22:12 +0300

    move stuff back to builtins.c for less boilerplate

--- src/builtins.c
+++ src/builtins.c
@@ -4,12 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ast.h"
-#include "builtins/arrayfunc.h"
-#include "builtins/catch.h"
-#include "builtins/operators.h"
-#include "builtins/print.h"
+#include "attribute.h"
+#include "check.h"
 #include "common.h"
+#include "equals.h"
 #include "interpreter.h"
+#include "method.h"
 #include "objectsystem.h"
 #include "objects/array.h"
 #include "objects/block.h"
@@ -22,6 +22,149 @@
 #include "objects/scope.h"
 #include "objects/string.h"
 #include "unicode.h"
+
+
+// this is partialled to a block of code to create array_funcs
+static struct Object *arrayfunc_runner(struct Interpreter *interp, struct Object *argarr)
+{
+	struct Object *parentscope = attribute_get(interp, ARRAYOBJECT_GET(argarr, 0), "definition_scope");
+	if (!parentscope)
+		return NULL;
+
+	struct Object *scope = scopeobject_newsub(interp, parentscope);
+	OBJECT_DECREF(interp, parentscope);
+	if (!scope)
+		return NULL;
+
+	struct Object *localvars = NULL, *string = NULL, *array = NULL;
+	if (!((localvars = attribute_get(interp, scope, "local_vars")) &&
+			(string = stringobject_newfromcharptr(interp, "arguments")) &&
+			(array = arrayobject_slice(interp, argarr, 1, ARRAYOBJECT_LEN(argarr))))) {
+		if (localvars) OBJECT_DECREF(interp, localvars);
+		if (string) OBJECT_DECREF(interp, string);
+		if (array) OBJECT_DECREF(interp, array);
+		OBJECT_DECREF(interp, scope);
+		return NULL;
+	}
+
+	int status = mappingobject_set(interp, localvars, string, array);
+	OBJECT_DECREF(interp, localvars);
+	OBJECT_DECREF(interp, string);
+	OBJECT_DECREF(interp, array);
+	if (status == STATUS_ERROR) {
+		OBJECT_DECREF(interp, scope);
+		return NULL;
+	}
+
+	struct Object *res = method_call(interp, ARRAYOBJECT_GET(argarr, 0), "run", scope, NULL);
+	OBJECT_DECREF(interp, scope);
+	return res;
+}
+
+static struct Object *array_func(struct Interpreter *interp, struct Object *argarr)
+{
+	if (check_args(interp, argarr, interp->builtins.blockclass, NULL) == STATUS_ERROR)
+		return NULL;
+	struct Object *block = ARRAYOBJECT_GET(argarr, 0);
+
+	struct Object *runnerobj = functionobject_new(interp, arrayfunc_runner);
+	if (!runnerobj)
+		return NULL;
+
+	struct Object *res = functionobject_newpartial(interp, runnerobj, block);
+	OBJECT_DECREF(interp, runnerobj);
+	return res;
+}
+
+
+static int run_block(struct Interpreter *interp, struct Object *block)
+{
+	struct Object *defscope = attribute_get(interp, block, "definition_scope");
+	if (!defscope)
+		return STATUS_ERROR;
+
+	struct Object *subscope = scopeobject_newsub(interp, defscope);
+	OBJECT_DECREF(interp, defscope);
+	if (!subscope)
+		return STATUS_ERROR;
+
+	struct Object *res = method_call(interp, block, "run", subscope, NULL);
+	OBJECT_DECREF(interp, subscope);
+	if (!res)
+		return STATUS_ERROR;
+
+	OBJECT_DECREF(interp, res);
+	return STATUS_OK;
+}
+
+static struct Object *catch(struct Interpreter *interp, struct Object *argarr)
+{
+	if (check_args(interp, argarr, interp->builtins.blockclass, interp->builtins.blockclass, NULL) == STATUS_ERROR)
+		return NULL;
+	struct Object *trying = ARRAYOBJECT_GET(argarr, 0);
+	struct Object *caught = ARRAYOBJECT_GET(argarr, 1);
+
+	if (run_block(interp, trying) == STATUS_ERROR) {
+		// TODO: make the error available somewhere instead of resetting it here?
+		assert(interp->err);
+		OBJECT_DECREF(interp, interp->err);
+		interp->err = NULL;
+
+		if (run_block(interp, caught) == STATUS_ERROR)
+			return NULL;
+	}
+
+	// everything succeeded or the error handling code succeeded
+	return interpreter_getbuiltin(interp, "null");
+}
+
+
+#define BOOL(interp, x) interpreter_getbuiltin((interp), (x) ? "true" : "false")
+struct Object *equals_builtin(struct Interpreter *interp, struct Object *argarr)
+{
+	if (check_args(interp, argarr, interp->builtins.objectclass, interp->builtins.objectclass, NULL) == STATUS_ERROR)
+		return NULL;
+
+	int res = equals(interp, ARRAYOBJECT_GET(argarr, 0), ARRAYOBJECT_GET(argarr, 1));
+	if (res == -1)
+		return NULL;
+
+	assert(res == !!res);
+	return BOOL(interp, res);
+}
+
+static struct Object *same_object(struct Interpreter *interp, struct Object *argarr)
+{
+	if (check_args(interp, argarr, interp->builtins.objectclass, interp->builtins.objectclass, NULL) == STATUS_ERROR)
+		return NULL;
+	return BOOL(interp, ARRAYOBJECT_GET(argarr, 0) == ARRAYOBJECT_GET(argarr, 1));
+}
+#undef BOOL
+
+
+static struct Object *print(struct Interpreter *interp, struct Object *argarr)
+{
+	if (check_args(interp, argarr, interp->builtins.stringclass, NULL) == STATUS_ERROR)
+		return NULL;
+
+	char *utf8;
+	size_t utf8len;
+	char errormsg[100];
+	int status = utf8_encode(*((struct UnicodeString *) ARRAYOBJECT_GET(argarr, 0)->data), &utf8, &utf8len, errormsg);
+	if (status == STATUS_NOMEM) {
+		errorobject_setnomem(interp);
+		return NULL;
+	}
+	assert(status == STATUS_OK);  // TODO: how about invalid unicode strings? make sure they don't exist when creating strings?
+
+	// TODO: avoid writing 1 byte at a time... seems to be hard with c \0 strings
+	for (size_t i=0; i < utf8len; i++)
+		putchar(utf8[i]);
+	free(utf8);
+	putchar('\n');
+
+	return interpreter_getbuiltin(interp, "null");
+}
 
 
 static int create_method_mapping(struct Interpreter *interp, struct Object *klass)
@@ -71,11 +214,11 @@ int builtins_setup(struct Interpreter *interp)
 	if (!(interp->builtins.scopeclass = scopeobject_createclass(interp))) goto error;
 	if (!(interp->builtins.blockclass = blockobject_createclass(interp))) goto error;
 
-	if (!(interp->builtins.array_func = functionobject_new(interp, builtin_arrayfunc))) goto error;
-	if (!(interp->builtins.catch = functionobject_new(interp, builtin_catch))) goto error;
-	if (!(interp->builtins.equals = functionobject_new(interp, builtin_equals))) goto error;
-	if (!(interp->builtins.print = functionobject_new(interp, builtin_print))) goto error;
-	if (!(interp->builtins.same_object = functionobject_new(interp, builtin_sameobject))) goto error;
+	if (!(interp->builtins.array_func = functionobject_new(interp, array_func))) goto error;
+	if (!(interp->builtins.catch = functionobject_new(interp, catch))) goto error;
+	if (!(interp->builtins.equals = functionobject_new(interp, equals_builtin))) goto error;
+	if (!(interp->builtins.print = functionobject_new(interp, print))) goto error;
+	if (!(interp->builtins.same_object = functionobject_new(interp, same_object))) goto error;
 
 	if (!(interp->builtinscope = scopeobject_newbuiltin(interp))) goto error;
 
--- src/builtins/catch.h
+++
@@ -1,9 +0,0 @@
-#ifndef BUILTINS_CATCH_H
-#define BUILTINS_CATCH_H
-
-struct Interpreter;
-struct Object;
-
-struct Object *builtin_catch(struct Interpreter *interp, struct Object *argarr);
-
-#endif    // BUILTINS_CATCH_H
