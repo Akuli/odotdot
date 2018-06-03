b90158b7601d047ef36ae3b30f8164148cd5ac06
Akuli <akuviljanen17@gmail.com>
2018-06-03 10:50:23 +0300

    quick cleanup

--- src/attribute.c
+++ src/attribute.c
@@ -1,6 +1,7 @@
 // TODO: test these, or just write a lot of code that uses them and test that
 #include "attribute.h"
 #include <assert.h>
+#include <stdbool.h>
 #include "common.h"
 #include "method.h"
 #include "unicode.h"
@@ -10,15 +11,29 @@
 #include "objects/mapping.h"
 #include "objects/string.h"
 
+// returns true on success and false on failure
+static bool init_mappings(struct Interpreter *interp, struct ClassObjectData *data)
+{
+	if (!data->getters) {
+		if (!(data->getters = mappingobject_newempty(interp)))
+			return false;
+	}
+	if (!data->setters) {
+		if (!(data->setters = mappingobject_newempty(interp)))
+			return false;
+	}
+	return true;
+}
+
 
 int attribute_add(struct Interpreter *interp, struct Object *klass, char *name, functionobject_cfunc getter, functionobject_cfunc setter)
 {
-	// this must not be called tooo early by builtins_setup()
-	assert(interp->builtins.mappingclass);
-	assert(interp->builtins.functionclass);
+	assert(interp->builtins.mappingclass && interp->builtins.functionclass);   // must not be called tooo early by builtins_setup()
 	assert(setter || getter);
 
 	struct ClassObjectData *data = klass->data;
+	if (!init_mappings(interp, data))
+		return STATUS_ERROR;
 
 	struct Object *string = stringobject_newfromcharptr(interp, name);
 	if (!string)
@@ -29,12 +44,6 @@ int attribute_add(struct Interpreter *interp, struct Object *klass, char *name, 
 		struct Object *getterobj = functionobject_new(interp, getter);
 		if (!getterobj)
 			goto error;
-		if (!data->getters) {
-			if (!(data->getters = mappingobject_newempty(interp))) {
-				OBJECT_DECREF(interp, getterobj);
-				goto error;
-			}
-		}
 		if (mappingobject_set(interp, data->getters, string, getterobj) == STATUS_ERROR) {
 			OBJECT_DECREF(interp, getterobj);
 			goto error;
@@ -45,12 +54,6 @@ int attribute_add(struct Interpreter *interp, struct Object *klass, char *name, 
 		struct Object *setterobj = functionobject_new(interp, setter);
 		if (!setterobj)
 			goto error;
-		if (!data->setters) {
-			if (!(data->setters = mappingobject_newempty(interp))) {
-				OBJECT_DECREF(interp, setterobj);
-				goto error;
-			}
-		}
 		if (mappingobject_set(interp, data->setters, string, setterobj) == STATUS_ERROR) {
 			OBJECT_DECREF(interp, setterobj);
 			goto error;
@@ -70,24 +73,21 @@ error:
 struct Object *attribute_getwithstringobj(struct Interpreter *interp, struct Object *obj, struct Object *stringobj)
 {
 	struct ClassObjectData *data = obj->klass->data;
-	if (!data->getters)
-		goto notfound;
+	if (!init_mappings(interp, data))
+		return NULL;
 
 	struct Object *getter = mappingobject_get(interp, data->getters, stringobj);
 	if (!getter) {
 		// mappingobject_get is shit, so we need more shit here
 		if (interp->err)
 			return NULL;
-		goto notfound;
+		errorobject_setwithfmt(interp, "%s objects have no attribute named %D", data->name, stringobj);
+		return NULL;
 	}
 
 	struct Object *res = functionobject_call(interp, getter, obj, NULL);
 	OBJECT_DECREF(interp, getter);
 	return res;
-
-notfound:
-	errorobject_setwithfmt(interp, "%s objects have no attribute named %D", data->name, stringobj);
-	return NULL;
 }
 
 struct Object *attribute_get(struct Interpreter *interp, struct Object *obj, char *attr)
@@ -105,15 +105,17 @@ struct Object *attribute_get(struct Interpreter *interp, struct Object *obj, cha
 int attribute_setwithstringobj(struct Interpreter *interp, struct Object *obj, struct Object *stringobj, struct Object *val)
 {
 	struct ClassObjectData *data = obj->klass->data;
-	if (!data->setters)
-		goto notfound;
+	if (!init_mappings(interp, data))
+		return STATUS_ERROR;
 
 	struct Object *setter = mappingobject_get(interp, data->setters, stringobj);
 	if (!setter) {
 		// mappingobject_get is shit, so we need more shit here
 		if (interp->err)
 			return STATUS_ERROR;
-		goto notfound;
+		// TODO: check if there's a getter for the attribute for better error messages
+		errorobject_setwithfmt(interp, "%s objects have no attribute named %D or it's read-only", data->name, stringobj);
+		return STATUS_ERROR;
 	}
 
 	struct Object *res = functionobject_call(interp, setter, obj, val, NULL);
@@ -122,11 +124,6 @@ int attribute_setwithstringobj(struct Interpreter *interp, struct Object *obj, s
 		return STATUS_ERROR;
 	OBJECT_DECREF(interp, res);
 	return STATUS_OK;
-
-notfound:
-	// TODO: check if there's a getter for the attribute for better error messages
-	errorobject_setwithfmt(interp, "%s objects have no attribute named %D or it's read-only", data->name, stringobj);
-	return STATUS_ERROR;
 }
 
 int attribute_set(struct Interpreter *interp, struct Object *obj, char *attr, struct Object *val)
